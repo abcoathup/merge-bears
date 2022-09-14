1b63966381cb86dc22dad8e0275341400f65c7ef
abcoathup <andrew@coathup.net>
2022-09-14 12:42:11 +1000

    fix warning

--- script/ConsensusLayerBear.s.sol
+++ script/ConsensusLayerBear.s.sol
@@ -11,7 +11,7 @@ contract ConsensusLayerBearScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        new ConsensusLayerBear();
+        ConsensusLayerBear token = new ConsensusLayerBear();
 
         vm.stopBroadcast();
     }
